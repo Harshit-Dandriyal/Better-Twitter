a71880fbaa3d29cabb2e6724efa738326d37cfc6
Harshit Dandriyal <56961488+Harshit-Dandriyal@users.noreply.github.com>
2023-01-21 06:52:39 +0530

    Fixed issues
    Signed-off-by: Harshit Dandriyal <56961488+Harshit-Dandriyal@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,43 @@ import Head from "next/head";
 import Feed from "../components/Feed";
 import Navigator from "../components/Navigator";
 import Widgets from "../components/Widgets";
-import { getProviders, getSession, useSession } from "next-auth/react";
+import { getProviders, getSession, signIn, useSession } from "next-auth/react";
 import Modal from "../components/Modal";
 import { editmodalState, modalState } from "../atoms/modalAtom";
 import { useRecoilState } from "recoil";
-import Login from "./auth/Login";
+import Image from "next/image";
 
 export default function Home({ trendingResults, followResults, providers }) {
   const { data: session } = useSession();
   const [isOpen, setIsOpen] = useRecoilState(modalState);
 
-  if (!session) return <>{console.log("fds", providers)}<Login providers={providers} /></>;
+  if (!session) return  <div className="flex flex-col items-center space-y-20 pt-48">
+      <Image
+        src="https://rb.gy/ogau5a"
+        width={150}
+        height={150}
+      />
 
+      <div>
+     
+      {Object?.values(providers)?.map((provider) => (
+          <div key={provider.name}>
+            {/* https://devdojo.com/tailwindcss/buttons#_ */}
+            <button
+              className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#1d9bf0] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+              <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
+                Sign in with {provider.name}
+              </span>
+            </button>
+          </div>
+      
+   
+    ))}
+   </div>
+   </div>
   return (
     <div className="">
       <Head>
