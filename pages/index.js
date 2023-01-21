fc33f2ff9719fe99e2a095a50ee308fb8e2b9345
Harshit Dandriyal <56961488+Harshit-Dandriyal@users.noreply.github.com>
2023-01-21 06:56:54 +0530

    Fixed issues
    Signed-off-by: Harshit Dandriyal <56961488+Harshit-Dandriyal@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -13,32 +13,26 @@ export default function Home({ trendingResults, followResults, providers }) {
   const [isOpen, setIsOpen] = useRecoilState(modalState);
 
   if (!session) return  <div className="flex flex-col items-center space-y-20 pt-48">
-      <Image
-        src="https://rb.gy/ogau5a"
-        width={150}
-        height={150}
-      />
+  <Image
+    src="https://rb.gy/ogau5a"
+    width={150}
+    height={150}
+  />
 
-      <div>
-     
-      {Object?.values(providers)?.map((provider) => (
-          <div key={provider.name}>
-            {/* https://devdojo.com/tailwindcss/buttons#_ */}
-            <button
-              className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
-              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
-            >
-              <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#1d9bf0] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
-              <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
-                Sign in with {provider.name}
-              </span>
-            </button>
-          </div>
-      
-   
-    ))}
-   </div>
-   </div>
+  <div>
+ 
+        {/* https://devdojo.com/tailwindcss/buttons#_ */}
+        <button
+          className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group"
+          onClick={() => signIn()}
+        >
+          <span className="w-48 h-48 rounded rotate-[-40deg] bg-[#1d9bf0] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
+          <span className="relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white">
+            Sign in with Google
+          </span>
+        </button>
+      </div>
+      </div>
   return (
     <div className="">
       <Head>
